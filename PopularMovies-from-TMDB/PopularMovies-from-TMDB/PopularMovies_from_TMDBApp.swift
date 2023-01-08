f3516d5158e397d816e1373b1ef14915d0b93d29
Dereje <derelife1@gmail.com>
2023-01-08 00:00:46 -0500

    Project set

---
+++ PopularMovies-from-TMDB/PopularMovies-from-TMDB/PopularMovies_from_TMDBApp.swift
@@ -0,0 +1,17 @@
+//
+//  PopularMovies_from_TMDBApp.swift
+//  PopularMovies-from-TMDB
+//
+//  Created by Consultant on 1/8/23.
+//
+
+import SwiftUI
+
+@main
+struct PopularMovies_from_TMDBApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
